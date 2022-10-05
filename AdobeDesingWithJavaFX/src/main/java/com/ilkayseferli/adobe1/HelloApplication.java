dd8d752b8e3f5b3796d580174559e94e68562dab
bymeg <ilkayseferli@gmail.com>
2022-10-05 18:21:04 +0300

    Custom title bar bug fix.

--- AdobeDesingWithJavaFX/src/main/java/com/ilkayseferli/adobe1/HelloApplication.java
+++ AdobeDesingWithJavaFX/src/main/java/com/ilkayseferli/adobe1/HelloApplication.java
@@ -13,23 +13,22 @@ public class HelloApplication extends Application {
     double x,y = 0;
     @Override
     public void start(Stage stage) throws IOException {
-        Parent root = FXMLLoader.load(HelloApplication.class.getResource("fxml/main.fxml"));
-        Scene scene = new Scene(root);
-        stage.setScene(scene);
+        Parent root = FXMLLoader.load(getClass().getResource("fxml/main.fxml"));
+        stage.setScene(new Scene(root));
         stage.initStyle(StageStyle.UNDECORATED);
         root.setOnMousePressed(evt -> {
             x = evt.getSceneX();
             y = evt.getSceneY();
         });
         root.setOnMouseDragged(evt -> {
-            stage.setX(evt.getSceneX() - x);
-            stage.setY(evt.getSceneY() - y);
+            stage.setX(evt.getScreenX());
+            stage.setY(evt.getScreenY());
         });
 
         stage.show();
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
